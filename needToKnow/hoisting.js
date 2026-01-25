b4caf1701a46eb4deebd80146383429dd320f930
Gaurav Pandey <mwc2024001@iiita.ac.in>
2026-01-25 23:16:18 +0000

    hoisting

---
+++ needToKnow/hoisting.js
@@ -0,0 +1,28 @@
+getName();
+
+// setName();   // TypeError: setName is not a function 
+// resetName();   // act same as setName
+
+
+console.log(x);   // result : undefined
+                  // if x is not defined in the code then => ReferenceError: x is not defined
+
+console.log(getName); // output the function => [Function: getName]
+
+var x = 7;
+function getName(){
+    console.log("Gaurav");
+}
+
+// getName();
+// console.log(x);
+// console.log(getName);
+
+
+var setName = () => {
+    console.log("Rishabh");
+}
+
+var resetName = function(){
+    console.log("Souvik");
+}
