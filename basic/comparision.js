e428591705867dbeb1fdff575154e1550db46574
Gaurav Pandey <mwc2024001@iiita.ac.in>
2026-01-25 16:16:32 +0000

    comparision

---
+++ basic/comparision.js
@@ -0,0 +1,13 @@
+// same as before
+console.log( 1 != 2)
+
+// while comparision make sure that your datatype is same
+
+console.log(null > 0)
+console.log(null == 0)
+console.log(null >= 0)  // ----- true in this case
+
+// equality check(==) and comarisons( >=, <=, >, <) work differently
+// comparisons convert null to number, treating it as 0;
+
+console.log(undefined >=0) // undefined ke case me false
