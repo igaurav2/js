2985fe788e109a04ca0419868de42512b473064f
Gaurav Pandey <mwc2024001@iiita.ac.in>
2026-01-26 10:31:09 +0000

    undefined vs not defined

--- basic/dataTypes.js
+++ basic/dataTypes.js
@@ -1,9 +1,39 @@
 "use strict"; // treat all JS code as modern JS
 
+
 // alert(3 + 4); // we are using nodejs, not browser
 
 // null; // empty value
-// undefined; // value is not assigned
+
+
+//----undefined vs not defined
+
+// undefined ==> value is not assigned
+                 // Output
+let a;
+console.log(a);  // undefined
+
+// console.log(b);  // undefined
+// var b = 4;
+
+// console.log(c);  // ReferenceError: Cannot access 'c' before initialization
+// let c = 8;
+
+
+// not defined
+console.log(p);  // ReferenceError: p is not defined
+
+
+
 
 console.log(typeof null); // object (this is a bug in JS, null is not an object)
 console.log(typeof undefined);  // undefined
+
+
+// js is a loosely typed language
+let undef;
+console.log(undef);
+undef = 10;
+console.log(undef);
+undef = "Gaurav";
+console.log(undef);
