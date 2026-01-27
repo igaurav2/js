b0f416e75050268867fafe0f6901fe1ac0e112c6
Gaurav Pandey <mwc2024001@iiita.ac.in>
2026-01-27 08:33:25 +0000

    more on functions

--- basic/function.js
+++ basic/function.js
@@ -1,20 +1,48 @@
-var x = 1;
-a();
-b();
-console.log(x);
+a();       //---> funA
+// b();    //---> TypeError: b is not a function  -->---------------------------------------------------
+                                                                                                      //
+/*---- Phase 1                                                                                        //
+    a: {....}                                                                                         //
+    b: undefined                                                                                      //
+*/                                                                                                    //
+                                                                                                      //
+function a() {                      // Function Statement aka Function Declaration                    //
+    console.log("functionStatement");                                                                              //
+}                                                                                                     //
+// ---- Output: 10 1                                                                                  //
+                                                                                                      //
+var b = function(){                 // Function Expression                                            //
+    console.log("functionExpression");                                                                              //
+}                                                                                                     //
+                                                                                                      //
+b();    //---> funB <-------------------------------------------------------------------------------<---
 
-function a() {
-    var x = 10;
-    console.log(x);
+
+
+var c = function namedfun() {       // namedFunction Expression
+    console.log("namedFunction");            //    |---> can be accessed inside a function
 }
+// namedfun();     //---> ReferenceError: namedfun is not defined
+c();    //---> funC
 
-function b() {
-    var x = 100;
-    console.log(x);
-}
 
-/*---- Phase 1
-    x: undefined
-    a: {....}
-    b: {....}
+
+/*--------------  Anonymous Function ==> function without a name
+
+    function () {}   //---> SyntaxError: Function statements require a function name
+    anonymousFunctions are used when function are used as values
 */
+
+
+// ------------ Parameters & Arguments
+var d = function (parameter1, parameter2){   // label and indentifiers which gets those values are parameters
+    console.log("parameter&Argument");
+}
+d("argument1", "argument2");    // arguments are values passed inside a function
+
+
+// ------------- First Class Functions ==> ability to use functions as values
+var e = function(){
+    return function firstClass () {}
+}
+console.log(e());   //---> [Function: firstClass]
