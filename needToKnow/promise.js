353d72c3e8ac0b8a34a8c98283e12c72b629e4e2
Gaurav Pandey <mwc2024001@iiita.ac.in>
2026-01-28 23:28:27 +0000

    promise api

--- needToKnow/promise.js
+++ needToKnow/promise.js
@@ -59,3 +59,20 @@ createOrder(cart)
         console.log(err);
     });
 
+
+// --------------- Promise Api
+
+const p1 = new Promise((resolve, reject) => {setTimeout(()=> resolve("P1 success"), 3000)});
+const p2 = new Promise((resolve, reject) => {setTimeout(()=> resolve("P2 success"), 1000)});
+const p3 = new Promise((resolve, reject) => {setTimeout(()=> resolve("P3 success"), 2000)});
+
+    Promise.all([ p1, p2, p3 ])           //   ==> Success : wait for all of them to finish
+    .then(res => console.log(res));   //---> [ 'P1 success', 'P2 success', 'P3 success' ]
+                                          //       Failure : as soon as first error occured
+                                    
+    // Promise.allSetteled([ p1, p2, p3 ])     ==> wait for all of them to finish
+
+    // Promise.race([ p1, p2, p3 ])            ==> value of the first settled promise
+                                        
+    // Promise.any([ p1, p2, p3 ])             ==> Success : wait for first success
+    //                                             Failure : aggregate error (list of all errors)                                
